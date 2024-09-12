30654921c2176c5d067a97515f783e6afbf3ffd2
raviycoder <135521192+raviycoder@users.noreply.github.com>
2024-09-12 18:58:48 +0530

    initial comment

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import "./App.css";
+import CustomCarousel from "./components/Carousel";
+import Divide from "./components/Divide";
+import CustomTabs from "./components/Tabs";
+
+function App() {
+  return (
+    <main className="px-6 m-auto py-3 min-h-screen">
+      <div className="grid grid-cols-4 gap-6 md:grid-cols-10 lg:grid-cols-12">
+        {" "}
+        <div className="col-span-2 lg:col-span-5"></div>
+        <div className="col-span-8 lg:col-span-7">
+          <CustomTabs />
+          <Divide/>
+          <CustomCarousel />
+          <Divide/>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { NextUIProvider } from "@nextui-org/react";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <NextUIProvider>
+      <main className="dark text-foreground bg-gradient-to-l from-[#373E44] to-[#191B1F]">
+        <App />
+      </main>
+    </NextUIProvider>
+  </StrictMode>
+);
