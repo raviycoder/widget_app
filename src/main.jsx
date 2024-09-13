b60f6dd0fdb5889ed97a6080e51276b9c2124526
raviycoder <135521192+raviycoder@users.noreply.github.com>
2024-09-13 13:21:17 +0530

    test 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { NextUIProvider } from "@nextui-org/react";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NextUIProvider>
-      <main className="dark text-foreground bg-gradient-to-l from-[#373E44] to-[#191B1F]">
-        <App />
-      </main>
-    </NextUIProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
